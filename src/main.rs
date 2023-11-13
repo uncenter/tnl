8670010ec1c109cb89ae48ace66dc6ee96a8ff6e
uncenter <47499684+uncenter@users.noreply.github.com>
2023-11-12 22:56:26 -0500

    feat: add dir walker

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
 use anyhow::{Context, Result};
 use clap::Parser;
+use ignore::WalkBuilder;
+use std::path::Path;
 
 #[derive(Parser)]
 struct Cli {
 	path: std::path::PathBuf,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let args = Cli::parse();
-
-	let file_contents = std::fs::read_to_string(&args.path)
-		.with_context(|| format!("could not read file `{}`", &args.path.display()))?;
+fn check_trailing_newline(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+	let file_contents = std::fs::read_to_string(path)
+		.with_context(|| format!("could not read file `{}`", path.display()))?;
 
 	if !file_contents.ends_with('\n') {
-		println!("{}", &args.path.display());
+		println!("{}", path.display());
 	}
 
 	Ok(())
+}
+
+fn main() {
+	let args = Cli::parse();
+
+	for result in WalkBuilder::new("./").hidden(false).build() {
+		match result {
+			Ok(entry) => check_trailing_newline(entry.path()),
+			Err(err) => println!("ERROR: {}", err),
+		}
+	}
 }
