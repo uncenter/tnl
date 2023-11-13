a175e3fa06f4c2c41834293784d9bd7e5a4d13fd
uncenter <47499684+uncenter@users.noreply.github.com>
2023-11-13 11:27:53 -0500

    feat: build out walkdir, add fix option

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,53 @@
-use anyhow::{Context, Result};
 use clap::Parser;
 use ignore::WalkBuilder;
-use std::path::Path;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 #[derive(Parser)]
+#[clap(name = "tnl")]
 struct Cli {
 	path: std::path::PathBuf,
+	#[clap(long, short)]
+    fix: bool,
 }
 
-fn check_trailing_newline(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
-	let file_contents = std::fs::read_to_string(path)
-		.with_context(|| format!("could not read file `{}`", path.display()))?;
-
-	if !file_contents.ends_with('\n') {
-		println!("{}", path.display());
-	}
-
-	Ok(())
+fn append_newline(path: &std::path::Path) -> std::io::Result<()> {
+    let mut file = OpenOptions::new().append(true).open(path)?;
+    writeln!(file)?;
+    Ok(())
 }
 
 fn main() {
 	let args = Cli::parse();
 
-	for result in WalkBuilder::new("./").hidden(false).build() {
+	for result in WalkBuilder::new(args.path.clone()).hidden(false).build() {
 		match result {
-			Ok(entry) => check_trailing_newline(entry.path()),
-			Err(err) => println!("ERROR: {}", err),
+			Ok(entry) => match std::fs::metadata(entry.path()) {
+				Ok(metadata) => {
+					if !metadata.is_dir() {
+						if let Ok(contents) = std::fs::read_to_string(entry.path()) {
+							if !contents.ends_with('\n') {
+								println!("{}", entry.path().display());
+								if args.fix {
+									if let Err(_err) = append_newline(entry.path()) {
+										eprintln!("Something went wrong while fixing file '{}'.", entry.path().display());
+									}
+								}
+							}
+						}
+					}
+				}
+				Err(_err) => {
+					eprintln!("Something went wrong while reading file '{}'.", entry.path().display());
+				}
+			},
+			Err(_err) => {
+				eprintln!(
+					"Could not read file or directory '{}'.",
+					args.path.to_string_lossy()
+				);
+				std::process::exit(1)
+			}
 		}
 	}
 }
