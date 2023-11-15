35f6a23ad4fb981f60507dd363b6c839a353b3bd
uncenter <47499684+uncenter@users.noreply.github.com>
2023-11-14 19:55:02 -0500

    impr: less nesting w/ .expect() error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use ignore::overrides::OverrideBuilder;
 use ignore::WalkBuilder;
-use std::fs::OpenOptions;
+use std::fs;
 use std::io::Write;
 
 #[derive(Parser)]
@@ -13,7 +13,7 @@ struct Cli {
 }
 
 fn append_newline(path: &std::path::Path) -> std::io::Result<()> {
-	let mut file = OpenOptions::new().append(true).open(path)?;
+	let mut file = fs::OpenOptions::new().append(true).open(path)?;
 	writeln!(file)?;
 	Ok(())
 }
@@ -30,39 +30,24 @@ fn main() {
 		.overrides(overrides.build().expect("WalkBuilder construction error"));
 
 	for result in builder.build() {
-		match result {
-			Ok(entry) => match std::fs::metadata(entry.path()) {
-				Ok(metadata) => {
-					if !metadata.is_dir() {
-						if let Ok(contents) = std::fs::read_to_string(entry.path()) {
-							if !contents.ends_with('\n') {
-								println!("{}", entry.path().display());
-								if args.fix {
-									if let Err(_err) = append_newline(entry.path()) {
-										eprintln!(
-											"Something went wrong while fixing file '{}'.",
-											entry.path().display()
-										);
-									}
-								}
-							}
-						}
+		let entry = result.expect(&format!(
+			"read file or directory '{}'",
+			args.path.to_string_lossy()
+		));
+		let metadata = fs::metadata(entry.path()).expect(&format!(
+			"read file or directory '{}'",
+			entry.path().display()
+		));
+		if !metadata.is_dir() {
+			if let Ok(contents) = fs::read_to_string(entry.path()) {
+				if !contents.ends_with('\n') {
+					println!("{}", entry.path().display());
+					if args.fix {
+						append_newline(entry.path())
+							.expect(&format!("fix file '{}'", entry.path().display()));
 					}
 				}
-				Err(_err) => {
-					eprintln!(
-						"Something went wrong while reading file '{}'.",
-						entry.path().display()
-					);
-				}
-			},
-			Err(_err) => {
-				eprintln!(
-					"Could not read file or directory '{}'.",
-					args.path.to_string_lossy()
-				);
-				std::process::exit(1)
 			}
-		}
+		};
 	}
 }
