ca907462fc45e91872f47081de484e4a0494cf08
uncenter <47499684+uncenter@users.noreply.github.com>
2023-11-21 09:00:54 -0500

    style: cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -30,10 +30,10 @@ fn main() {
 		.overrides(overrides.build().expect("WalkBuilder construction error"));
 
 	for result in builder.build() {
-		let entry = result.unwrap_or_else(|_| panic!("read file or directory '{}'",
-			args.path.to_string_lossy()));
-		let metadata = fs::metadata(entry.path()).unwrap_or_else(|_| panic!("read file or directory '{}'",
-			entry.path().display()));
+		let entry = result
+			.unwrap_or_else(|_| panic!("read file or directory '{}'", args.path.to_string_lossy()));
+		let metadata = fs::metadata(entry.path())
+			.unwrap_or_else(|_| panic!("read file or directory '{}'", entry.path().display()));
 		if !metadata.is_dir() {
 			if let Ok(contents) = fs::read_to_string(entry.path()) {
 				if !contents.ends_with('\n') {
