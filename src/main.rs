91bf8e35c302701f0ca0ee4790d0e51362588fff
uncenter <47499684+uncenter@users.noreply.github.com>
2023-11-21 11:01:33 -0500

    feat: handle errors with color-eyre

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use color_eyre::eyre::{Context, Result};
 use ignore::overrides::OverrideBuilder;
 use ignore::WalkBuilder;
 use std::fs;
@@ -12,38 +13,54 @@ struct Cli {
 	fix: bool,
 }
 
-fn append_newline(path: &std::path::Path) -> std::io::Result<()> {
-	let mut file = fs::OpenOptions::new().append(true).open(path)?;
-	writeln!(file)?;
+fn append_newline(path: &std::path::Path) -> Result<()> {
+	let mut file = fs::OpenOptions::new()
+		.append(true)
+		.open(path)
+		.with_context(|| format!("failed to open file '{}'", path.display()))?;
+	writeln!(file)
+		.with_context(|| format!("failed to write newline to file '{}'", path.display()))?;
 	Ok(())
 }
 
-fn main() {
+fn main() -> Result<()> {
+	color_eyre::install()?;
+
 	let args = Cli::parse();
 
 	let mut overrides = OverrideBuilder::new(args.path.clone());
-	overrides.add("!.git/").expect("Invalid exclude pattern");
+	overrides.add("!.git/").context("Invalid exclude pattern")?;
 
 	let mut builder = WalkBuilder::new(args.path.clone());
-	builder
-		.hidden(false)
-		.overrides(overrides.build().expect("WalkBuilder construction error"));
+	builder.hidden(false).overrides(
+		overrides
+			.build()
+			.context("WalkBuilder construction error")?,
+	);
 
 	for result in builder.build() {
-		let entry = result
-			.unwrap_or_else(|_| panic!("read file or directory '{}'", args.path.to_string_lossy()));
-		let metadata = fs::metadata(entry.path())
-			.unwrap_or_else(|_| panic!("read file or directory '{}'", entry.path().display()));
+		let entry = result.with_context(|| {
+			format!("could not read file or directory '{}'", args.path.display())
+		})?;
+		let metadata = fs::metadata(entry.path()).with_context(|| {
+			format!(
+				"could not read file or directory '{}'",
+				entry.path().display()
+			)
+		})?;
 		if !metadata.is_dir() {
 			if let Ok(contents) = fs::read_to_string(entry.path()) {
 				if !contents.ends_with('\n') {
 					println!("{}", entry.path().display());
 					if args.fix {
-						append_newline(entry.path())
-							.unwrap_or_else(|_| panic!("fix file '{}'", entry.path().display()));
+						append_newline(entry.path()).with_context(|| {
+							format!("could not fix file '{}'", entry.path().display())
+						})?;
 					}
 				}
 			}
 		};
 	}
+
+	Ok(())
 }
